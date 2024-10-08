2de3e239d844426d68356b292dae2fda3f2d19e5
sammy@fuse <sammysh@msn.com>
2024-10-08 20:42:21 +0100

    all working locally with onion server

--- fonion/app.py
+++ fonion/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -48,6 +48,21 @@ def index():
     return render_template('index.html', box1=box1_values, box2=box2_values, box3=box3_values, box4=box4_values)
 
 
+@app.route('/get_data')
+def get_data():
+    box1 = db_session.query(SensorData).filter_by(BoxID=1).order_by(SensorData.id.desc()).first()
+    box2 = db_session.query(SensorData).filter_by(BoxID=2).order_by(SensorData.id.desc()).first()
+    box3 = db_session.query(SensorData).filter_by(BoxID=3).order_by(SensorData.id.desc()).first()
+    box4 = db_session.query(SensorData).filter_by(BoxID=4).order_by(SensorData.id.desc()).first()
+
+    data = {
+        "box1": box1.CO2 if box1 is not None else 'Error',
+        "box2": box2.CO2 if box2 is not None else 'Error',
+        "box3": box3.CO2 if box3 is not None else 'Error',
+        "box4": box4.CO2 if box4 is not None else 'Error'
+    }
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
